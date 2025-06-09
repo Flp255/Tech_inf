ee0b16a7f7996b2804624dd1d86e6f1a912e833b
Flp255 <255414@edu.p.lodz.pl>
2025-06-09 10:12:22 +0200

    Poprawka czwartego podejscia :D

--- 02.06_klasy/Point.cpp
+++ 02.06_klasy/Point.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Point.h"
+#include <math.h>
 int Point::m_numberOfPoints = 0;
 Point::Point() {
     m_name = "bez nazwy";
@@ -16,7 +17,27 @@ Point::Point(const std::string& name, double x, double y) {
 Point::~Point() {
     Point::m_numberOfPoints--;
 }
-…
+void Point::setName(const std::string& name) { 
+m_name = name; 
+} 
+void Point::setXY(double x, double y) { 
+m_x = x; m_y = y; 
+}
+double Point::getX() { 
+    return m_x; }
+double Point::getY() { 
+    return m_y; }
+void Point::show() const { 
+    std::cout << "Point: " << m_name << "(" << m_x << ", " << m_y << ")" << std::endl; 
+}
+double Point::distance(const Point& p) const { 
+    return sqrt( (p.m_x- m_x) * (p.m_x- m_x) + (p.m_y- m_y) * (p.m_y- m_y) ); 
+}
+const Point& Point::distant(const Point& p) const { 
+double d = sqrt( m_x * m_x + m_y * m_y); double dp = sqrt(p.m_x * p.m_x + p.m_y * p.m_y);
+if (d > dp) return *this; 
+    return p; 
+}
 int Point::numberOfPoints() {
     return Point::m_numberOfPoints;
 }
